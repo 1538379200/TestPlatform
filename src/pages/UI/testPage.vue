acac9712091c53e41007c90e9d5a45ddd160229b
1538379200 <1538379200@qq.com>
2021-12-31 01:40:41 +0800

    测试页面增加步骤条

--- src/pages/UI/testPage.vue
+++ src/pages/UI/testPage.vue
@@ -2,7 +2,7 @@
   <n-alert closable type="info" title="炒鸡温柔的提示" v-if="tipShow" show-icon :on-after-leave="tipOut">你以为刷新界面我就会重新运行？
   </n-alert>
   <div>
-    <h2>{{ $route.params.caseFile }}</h2>
+<!--    <h2>{{ $route.params.caseFile }}</h2>-->
     <n-progress
         type="line"
         status="error"
@@ -13,16 +13,32 @@
     >
       用例执行-
       <n-tag type="success" round>
-        {{persentNum}}%
+        {{ persentNum }}%
       </n-tag>
     </n-progress>
+    <n-grid x-gap="12" :cols="2" style="padding: 5rem 2rem 0 2rem">
+      <n-gi :span="1">
+        <n-steps size="medium" vertical v-for="(item,index) in stepValues" :key="index">
+          <n-step :title=item.title :description=item.description :status=item.stepStatus @click="showModal = true"></n-step>
+          <template #error-icon>
+            <n-icon>
+              <sad-cry></sad-cry>
+            </n-icon>
+          </template>
+        </n-steps>
+      </n-gi>
+      <n-gi :span="1">
+        <div>占位</div>
+      </n-gi>
+    </n-grid>
   </div>
 </template>
 
 <script setup>
 import {useRoute} from 'vue-router'
 import {ref} from 'vue'
-import {NAlert,NProgress,NTag} from 'naive-ui'
+import {NAlert, NProgress, NTag, NSteps, NStep, NGrid, NGi,NIcon,NModal,NPopover} from 'naive-ui'
+import {SadCry} from '@vicons/fa'
 
 const $route = useRoute()
 
@@ -40,6 +56,17 @@ if ($route.params.caseFile) {
 //设置进度条百分比
 //标签文字也使用了此百分比进行显示
 const persentNum = ref(50)
+
+//设置自动添加执行步骤
+//步骤的stepStatus会自动给出显示状态
+const stepValues = [
+  {title: '第一个', description: '第一个介绍(这里显示运行的日志)', stepStatus: 'finish'},
+  {title: '第二个', description: '第二个介绍', stepStatus: 'error'},
+  {title: '第三个', description: '第三个介绍', stepStatus: 'error'},
+]
+
+//定义错误类型可以弹窗显示错误日志
+const showModal = ref(false)
 </script>
 
 <style scoped>
